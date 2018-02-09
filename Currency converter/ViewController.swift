827506f2a0bcfb8f861b0f52503b7f7b0716a3fb
Blincov Sergey <sblin180@gmail.com>
2018-02-09 13:26:24 +0300

    Changed text on label during startup

--- Currency converter/ViewController.swift
+++ Currency converter/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.label.text = "Тут будет курс"
     }
 
     override func didReceiveMemoryWarning() {
