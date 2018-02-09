c11ac3d290d58768b188c09903df77546a8d007e
Blincov Sergey <sblin180@gmail.com>
2018-02-09 17:40:17 +0300

    Added checking internet connection

---
+++ Currency converter/Reachability.swift
@@ -0,0 +1,32 @@
+//
+//  Reachability.swift
+//  Currency converter
+//
+//  This class check reachability of system components
+//
+//  Created by Блинцов Сергей on 09/02/2018.
+//  Copyright © 2018 Блинцов Сергей. All rights reserved.
+//
+
+import SystemConfiguration
+
+class Reachability {
+    
+    // Check internet connection
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                zeroSockAddress in SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)}
+        } ) else {
+            return false
+        }
+        var flags : SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {return false}
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
+    }
+}
